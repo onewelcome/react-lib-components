f9d5e29fcbf3998580ccc32f058516b2f6869f63
Daniel <daanf@live.nl>
2022-01-18 13:35:09 +0100

    UCL 1 Removed args parameter

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,14 +1,3 @@
-// import React, { FC, HTMLAttributes, ReactChild } from 'react';
-
-// export interface Props extends HTMLAttributes<HTMLDivElement> {
-//   children?: ReactChild;
-// }
-
-// /** This will render a button with children. */
-// export const Button: FC<Props> = ({ children }) => {
-//   return <button>{children}</button>;
-// };
-
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -10,7 +10,7 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = (args) => <Button>Text</Button>;
+const Template: Story<Props> = () => <Button>Text</Button>;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
