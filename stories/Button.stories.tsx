ed5ce17370f67e30cee42a3c7b6ac5c96104fcee
Daniel <daanf@live.nl>
2022-01-18 13:25:24 +0100

    UCL-1 Initial commit with TSDX

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Props } from '../src/Button';
+import Button from '../src/Button';
+
+const meta: Meta = {
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+
+const Template: Story<Props> = (args) => <Button>Text</Button>;
+
+// By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
+// https://storybook.js.org/docs/react/workflows/unit-testing
+export const Default = Template.bind({});
+
+Default.args = {};
