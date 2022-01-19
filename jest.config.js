14aa3a943e7696379a050e439dfa393ec2c741cb
Daniel <daanf@live.nl>
2022-01-19 17:08:26 +0100

    Added comment

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
+/**
+ * Identity obj proxy is needed for jest to be able to properly import scss files.
+ */
 module.exports = {
   moduleNameMapper: {
     '^.+\\.(css|scss)$': 'identity-obj-proxy',
