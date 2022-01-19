bb17657ee366c985ad318fe8359fc49041956c72
Daniel <daanf@live.nl>
2022-01-19 14:18:58 +0100

    Added prevent assignment and scss module support

---
+++ tsdx.config.js
@@ -0,0 +1,26 @@
+const replace = require('@rollup/plugin-replace');
+// const scss = require('rollup-plugin-scss');
+// const postcss = require('rollup-plugin-postcss');
+// const autoprefixer = require('autoprefixer');
+const styles = require('rollup-plugin-styles');
+
+module.exports = {
+  rollup(config, opts) {
+    config.plugins = config.plugins.map((p) =>
+      p.name === 'replace'
+        ? replace({
+            'process.env.NODE_ENV': JSON.stringify(opts.env),
+            preventAssignment: true,
+          })
+        : p
+    );
+
+    config.plugins.push(
+      styles({
+        modules: true,
+      })
+    );
+
+    return config;
+  },
+};
