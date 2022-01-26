bb9b9900525e5ebb126f7a3e5258c0dc3ddcb5c3
Daniel <daanf@live.nl>
2022-01-26 11:33:24 +0100

    UCL-4 Jest config now has setup file to include jest-dom

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
  * Identity obj proxy is needed for jest to be able to properly import scss files.
  */
 module.exports = {
+  setupFilesAfterEnv: ['./setupTests.ts'],
   moduleNameMapper: {
     '^.+\\.(css|scss)$': 'identity-obj-proxy',
   },
