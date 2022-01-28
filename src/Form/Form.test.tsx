9fb3dcca66206001505c7f4deecc68809df1d87f
Daniel <daanf@live.nl>
2022-01-28 09:42:59 +0100

    UCL-12 Added form props and tests

--- src/Form/Form.test.tsx
+++ src/Form/Form.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Form } from './Form';
 import { render } from '@testing-library/react';
+import { Button } from '../Button/Button';
+import userEvent from '@testing-library/user-event';
 
 describe('Form should render', () => {
   it('renders without crashing', () => {
@@ -10,4 +12,38 @@ describe('Form should render', () => {
     const component = getByTestId('component');
     expect(component).toBeDefined();
   });
+});
+
+describe('Event should fire', () => {
+  it('should fire the onSubmitHandler function', () => {
+    const onSubmitHandler = jest.fn();
+
+    onSubmitHandler.mockImplementation((event) => {
+      event.preventDefault();
+    });
+
+    const { getByTestId } = render(
+      <Form onSubmit={onSubmitHandler}>
+        <Button data-testid="submit-button" type="submit"></Button>
+      </Form>
+    );
+
+    const button = getByTestId('submit-button');
+    userEvent.click(button);
+    expect(onSubmitHandler).toHaveBeenCalled();
+  });
+});
+
+describe('Should be able to set some default form properties', () => {
+  const { getByTestId } = render(
+    <Form data-testid="form" method="POST" action="./test.php">
+      Test
+    </Form>
+  );
+
+  const form = getByTestId('form');
+
+  expect(form.getAttribute('method')).toBe('POST');
+  expect(form.getAttribute('action')).toBe('./test.php');
+  expect(form.innerHTML).toBe('Test');
 });
--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -2,6 +2,11 @@ import React, { HTMLAttributes, ReactChild } from 'react';
 
 export interface Props extends HTMLAttributes<HTMLFormElement> {
   children?: ReactChild;
+  method?: 'POST' | 'GET' | 'post' | 'get'; // Can't add a regular expression, nor capitalisation ignore.
+  action?: string;
+  onSubmit?: (event: React.FormEvent) => void;
+  onReset?: (event: React.FormEvent) => void;
+  onChange?: (event: React.FormEvent) => void;
 }
 
 export const Form = ({ children, ...rest }: Props) => {
