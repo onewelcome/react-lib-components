7539c4715481209f4fef9bd0fe873c8fd60c7a69
Daniel <daanf@live.nl>
2022-01-28 10:03:29 +0100

    UCL-6 Basic definition for the input element

--- src/Form/Input/Input.tsx
+++ src/Form/Input/Input.tsx
@@ -1,9 +1,30 @@
-import React, { HTMLAttributes, ReactChild } from 'react';
+import React, { HTMLAttributes } from 'react';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
+type text =
+  | 'text'
+  | 'email'
+  | 'file'
+  | 'number'
+  | 'password'
+  | 'search'
+  | 'tel'
+  | 'time'
+  | 'url'
+  | 'datetime'
+  | 'hidden';
+
+export interface Props extends HTMLAttributes<HTMLInputElement> {
+  text: text;
+  name: string;
+  value?: string;
+  id?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  onKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
 }
 
-export const Input = ({ children, ...rest }: Props) => {
-  return <div {...rest}>{children} </div>;
+export const Input = ({ ...rest }: Props) => {
+  return <input {...rest} />;
 };
