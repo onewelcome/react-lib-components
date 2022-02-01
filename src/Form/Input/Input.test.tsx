ed8eae13ecbf16694e8ba83d4daccf6d1ac0e3c6
Daniel <daanf@live.nl>
2022-02-01 11:29:04 +0100

    UCL-6 Updated input test to reflect new jsx structure

--- src/Form/Input/Input.test.tsx
+++ src/Form/Input/Input.test.tsx
@@ -63,7 +63,7 @@ describe('Should utilize validation function', () => {
       onValidationError: onValidationError,
     });
 
-    expect(input.parentElement).toHaveClass('error');
+    expect(input).toHaveClass('error');
     expect(errorMsg).toBe('Please remove the iWelcome and onegini reference.');
   });
 
@@ -100,7 +100,7 @@ describe('Should utilize validation function', () => {
       onValidationError: onValidationError,
     });
 
-    expect(input.parentElement).toHaveClass('error');
+    expect(input).toHaveClass('error');
     expect(errorMsg).toBe('This should fail');
   });
 });
