22bede76548eded29b76bc6d73bc3f98cdc2619a
Daniel <daanf@live.nl>
2022-02-03 10:55:17 +0100

    UCL-17 create select component

--- src/Form/Select/Select.tsx
+++ src/Form/Select/Select.tsx
@@ -1,9 +1,143 @@
-import React, { HTMLAttributes, ReactChild } from 'react';
+import { Option } from './Option';
+import classes from './Select.module.scss';
+
+import React, {
+  Fragment,
+  HTMLAttributes,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { Input } from '../Input/Input';
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
+  children: ReactElement[];
+  disabled?: boolean;
+  name?: string;
+  id?: string;
+  labeledBy: string;
+  placeholder: string;
+  error: boolean;
+  onSelectChange?: (option: { label: string; value: string }) => void;
 }
 
-export const Select = ({ children, ...rest }: Props) => {
-  return <div {...rest}>{children} </div>;
+export const Select = ({
+  children,
+  labeledBy,
+  placeholder = 'Choose an option',
+  error = false,
+  onSelectChange,
+  ...rest
+}: Props) => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<{
+    label: string;
+    value: string;
+  }>({ label: '', value: '' });
+  const [filter, setFilter] = useState<string>('');
+
+  const onOptionChangeHandler = (option: { label: string; value: string }) => {
+    setSelectedOption({ label: option.label, value: option.value });
+    setExpanded(false);
+  };
+
+  const handleSelectCloseOnBodyClick = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      if (!(event.target as Element).closest('.custom-select') && expanded) {
+        setExpanded(false);
+      }
+    },
+    [expanded]
+  );
+
+  useEffect(() => {
+    onSelectChange && onSelectChange(selectedOption);
+  }, [selectedOption]);
+
+  /**
+   * We need to make sure that the only children nested in this component are from the Option component, because we add props to the children.Î
+   */
+  useEffect(() => {
+    for (let child of children) {
+      if (child.type !== Option) {
+        throw new Error(
+          'Child element of <Select /> component needs to be <Option /> component'
+        );
+      }
+    }
+  }, []);
+
+  /**
+   * Add body click listener to close select and remove it in the cleanup function whenever expanded state changes.
+   */
+  useEffect(() => {
+    window.addEventListener('click', handleSelectCloseOnBodyClick);
+
+    return () => {
+      window.removeEventListener('click', handleSelectCloseOnBodyClick);
+    };
+  }, [expanded]);
+
+  /**
+   * @description We have to modify the children (Option component) to have a additional props that allows us to keep track of which one is selected at all times and if a filter is active.
+   * The `children` prop can be either a single object (1 child) or an array of multiple children.
+   */
+  const renderOptions = () => {
+    return children?.map((child, index) => (
+      <Fragment key={index}>
+        {React.cloneElement(child, {
+          onOptionSelect: onOptionChangeHandler,
+          selected: selectedOption,
+          filter: filter,
+        })}
+      </Fragment>
+    ));
+  };
+
+  const renderSearch = () => {
+    return (
+      <Input
+        onChange={filterResults}
+        className={classes['select-search']}
+        type="search"
+        name="search-option"
+        placeholder="Search item"
+      />
+    );
+  };
+
+  const filterResults = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.currentTarget.value);
+  };
+
+  return (
+    <div
+      {...rest}
+      className={`${classes.select} ${expanded ? classes.expanded : ''} ${
+        error ? classes.error : ''
+      } custom-select`}
+    >
+      <button
+        onClick={setExpanded.bind(null, !expanded)}
+        aria-expanded={expanded}
+        aria-haspopup="listbox"
+        aria-labelledby={labeledBy}
+      >
+        <span className={classes.selected}>
+          {selectedOption.value.length === 0 && (
+            <span className={classes.placeholder}>{placeholder}</span>
+          )}
+          {selectedOption.label}
+        </span>
+      </button>
+      {expanded && (
+        <ul tabIndex={-1} role="listbox">
+          {children.length > 10 && renderSearch()}
+          {renderOptions()}
+        </ul>
+      )}
+    </div>
+  );
 };
---
+++ stories/Form/Select.stories.tsx
@@ -0,0 +1,35 @@
+import React, { Fragment, useState } from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Select, Props } from '../../src/Form/Select/Select';
+import { Option } from '../../src/Form/Select/Option';
+
+const meta: Meta = {
+  title: 'Select',
+  component: Select,
+};
+
+export default meta;
+
+const Template: Story<Props> = (args) => {
+  return (
+    <Fragment>
+      <Select {...args}>
+        <Option value="option1">Test</Option>
+        <Option value="option2">Test2</Option>
+        <Option value="option3">Test3</Option>
+        <Option value="option4">Test4</Option>
+        <Option value="option5">Test5</Option>
+        <Option value="option6">Test6</Option>
+        <Option value="option7">Test7</Option>
+        <Option value="option8">Test8</Option>
+        <Option value="option9">Test9</Option>
+        <Option value="option10">Test10</Option>
+        <Option value="option11">Test11</Option>
+      </Select>
+    </Fragment>
+  );
+};
+
+export const SelectEl = Template.bind({});
+
+SelectEl.args = {};
