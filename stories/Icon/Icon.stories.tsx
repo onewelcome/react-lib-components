b646b7a810415acac7ddf16ea60aa4d4c3780ebb
Daniel <daanf@live.nl>
2022-02-07 13:00:25 +0100

    UCL-23 added h1 tag and small todo comment.

--- stories/Icon/Icon.stories.tsx
+++ stories/Icon/Icon.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Icon as IconComponent, Props, Icons } from '../../src/Icon/Icon';
 
@@ -9,6 +9,10 @@ const meta: Meta = {
 
 export default meta;
 
+/**
+ * Click the icon name to copy the React JSX code <Icon icon="icon_name" />
+ *
+ */
 const Template: Story<Props> = (args) => {
   const copyIconHandler = (icon) => {
     const toCopy = `<Icon icon="${Icons[icon]}" />`;
@@ -16,38 +20,46 @@ const Template: Story<Props> = (args) => {
     navigator.clipboard.writeText(toCopy);
   };
 
+  //@TODO: change h1 to <Typography /> component in UCL-5
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        flexWrap: 'wrap',
-      }}
-    >
-      {Object.keys(Icons).map((icon, index) => (
-        <article
-          key={index}
-          style={{
-            flexBasis: '20%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginBottom: '20px',
-          }}
-        >
-          <IconComponent {...args} icon={Icons[icon]} />
-          <button
-            onClick={copyIconHandler.bind(null, icon)}
-            title="Click here to copy React Component"
-            style={{ padding: '5px', cursor: 'copy', marginTop: '5px' }}
+    <Fragment>
+      <h1 style={{ textAlign: 'center' }}>
+        Click on the icon name to copy the JSX React Component (
+        <code>&lt;Icon icon="icon_name" /&gt;</code>)
+      </h1>
+      <br />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+        }}
+      >
+        {Object.keys(Icons).map((icon, index) => (
+          <article
+            key={index}
+            style={{
+              flexBasis: '20%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              marginBottom: '20px',
+            }}
           >
-            <pre style={{ margin: '0' }}>
-              <code>{Icons[icon]}</code>
-            </pre>
-          </button>
-        </article>
-      ))}
-    </div>
+            <IconComponent {...args} icon={Icons[icon]} />
+            <button
+              onClick={copyIconHandler.bind(null, icon)}
+              title="Click here to copy React Component"
+              style={{ padding: '5px', cursor: 'copy', marginTop: '5px' }}
+            >
+              <pre style={{ margin: '0' }}>
+                <code>{Icons[icon]}</code>
+              </pre>
+            </button>
+          </article>
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
