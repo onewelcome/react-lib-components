3b8e34b661145b38ffaecf1595a57fca349260b8
Daniel <daanf@live.nl>
2022-02-08 16:35:01 +0100

    UCL-43 Added default font family

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import { BaseStyling } from '../src/_BaseStyling_/BaseStyling';
+import './base.scss';
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
@@ -8,7 +9,11 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <BaseStyling>
+    <BaseStyling
+      properties={{
+        fontFamily: "'Red Hat Display', sans-serif;",
+      }}
+    >
       <Story />
     </BaseStyling>
   ),
