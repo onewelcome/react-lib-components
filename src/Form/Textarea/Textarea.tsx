a124ec4d94d69e35b22399f755bc7d4488ac3372
Daniel <daanf@live.nl>
2022-02-10 13:42:00 +0100

    UCL-18 we will have to work with wrappers, tried everything for warning icon

--- src/Form/Textarea/Textarea.tsx
+++ src/Form/Textarea/Textarea.tsx
@@ -1,12 +1,17 @@
 import React, { TextareaHTMLAttributes } from "react";
+import { Icon, Icons } from "../..";
 import classes from "./Textarea.module.scss";
 
 export interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   error?: boolean;
+  disabled?: boolean;
 }
 
-export const Textarea = ({ children, error = false, disabled, rows = 4, ...rest }: Props) => {
+export const Textarea = ({ children, error = false, disabled = false, rows = 4, ...rest }: Props) => {
   return (
-    <textarea rows={rows} className={`${classes.textarea} ${error ? classes.error : ""} ${disabled ? classes.disabled : ""}`} {...rest} />
+    <div className={`${classes["textarea-wrapper"]}`}>
+      <textarea rows={rows} className={`${error ? classes.error : ""} ${classes.textarea}`} disabled={disabled} {...rest} />
+      {error && <Icon className={classes.warning} icon={Icons.Warning} />}
+    </div>
   );
 };
