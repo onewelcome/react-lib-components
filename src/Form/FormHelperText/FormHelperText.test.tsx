93841c31407dadd87ed86ca880dc2dad7dbd2632
Daniel <daanf@live.nl>
2022-03-14 11:29:14 +0100

    UCL-12 a lot of work

--- src/Form/FormHelperText/FormHelperText.test.tsx
+++ src/Form/FormHelperText/FormHelperText.test.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { FormHelperText } from "./FormHelperText";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { FormHelperText } from './FormHelperText';
+import { render } from '@testing-library/react';
 
-describe("FormHelperText should render", () => {
-  it("renders without crashing", () => {
-    const { getByTestId } = render(<FormHelperText data-testid="component">This is a field description</FormHelperText>);
-    const component = getByTestId("component");
+describe('FormHelperText should render', () => {
+  it('renders without crashing', () => {
+    const { getByTestId } = render(
+      <FormHelperText data-testid="component">This is a field description</FormHelperText>
+    );
+    const component = getByTestId('component');
     expect(component).toBeDefined();
   });
 });
 
-describe("Attributes", () => {
-  it("contains the error class", () => {
+describe('Attributes', () => {
+  it('contains the error class', () => {
     const { getByTestId } = render(
       <FormHelperText error={true} data-testid="component">
         This is a field description
       </FormHelperText>
     );
-    const component = getByTestId("component");
+    const component = getByTestId('component');
 
-    expect(component).toHaveClass("error");
-    expect(component).toHaveStyle({ color: "rgb(226, 42, 29);" });
+    expect(component).toHaveClass('error');
+    expect(component).toHaveStyle({ color: 'rgb(226, 42, 29);' });
   });
 });
