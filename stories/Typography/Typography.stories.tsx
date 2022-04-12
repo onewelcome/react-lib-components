1bdfe04d4f17c0e568176a31a4b8897165e570bc
Daniel <daanf@live.nl>
2022-04-12 12:51:17 +0200

    UCL-122 Changed the way we do documentation.

--- stories/Typography/Typography.stories.tsx
+++ stories/Typography/Typography.stories.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Typography, Props } from '../../src/Typography/Typography';
+import TypographyDocumentation from './Typography.mdx';
 
 const meta: Meta = {
   title: 'Stories/UI/Typography',
   component: Typography,
+  parameters: {
+    docs: {
+      page: TypographyDocumentation,
+    },
+  },
 };
 
 export default meta;
@@ -13,4 +19,6 @@ const Template: Story<Props> = (args) => <Typography {...args}>content</Typograp
 
 export const TypographyEl = Template.bind({});
 
-TypographyEl.args = {};
+TypographyEl.args = {
+  variant: 'h1',
+};
