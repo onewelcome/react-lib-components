189e22d7f160a5c87cf0a73ac5807a4c51eed22c
Daniel <daanf@live.nl>
2022-05-02 09:22:42 +0200

    UCL-95 fixed toggle disabled state and helper position

--- src/Form/Toggle/Toggle.tsx
+++ src/Form/Toggle/Toggle.tsx
@@ -7,13 +7,22 @@ export interface Props
   children: string;
 }
 
-export const Toggle = ({ children, checked, ...rest }: Props) => (
+export const Toggle = ({ children, checked, disabled, ...rest }: Props) => (
   <div className={classes['toggle-wrapper']}>
-    <Checkbox {...rest} checked={checked} className={classes.checkbox} label={children}>
+    <Checkbox
+      {...rest}
+      checked={checked}
+      className={classes['checkbox']}
+      helperProps={{ className: classes['toggle-helper'] }}
+      disabled={disabled}
+      label={children}
+    >
       <span
         data-toggle
         aria-hidden="true"
-        className={`${classes.toggle} ${checked ? classes.checked : ''} `}
+        className={`${classes['toggle']} ${checked ? classes['checked'] : ''} ${
+          disabled ? classes['disabled'] : ''
+        } `}
       ></span>
     </Checkbox>
   </div>
