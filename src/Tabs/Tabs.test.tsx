4051cbe186a2043380b7400572926475d79bac39
Dirk Stals <dirkstals@gmail.com>
2022-05-19 16:48:57 +0200

    UCL-27 Add accessibility test

--- src/Tabs/Tabs.test.tsx
+++ src/Tabs/Tabs.test.tsx
@@ -132,7 +132,7 @@ describe('Tabs should render', () => {
 describe('Tabs should not render other children then tablist or tabpanels', () => {
   it('renders no tabs', () => {
     const queries = render(
-      <Tabs {...defaultParams} data-testid="tabs">
+      <Tabs data-testid="tabs">
         <Tab>wrong component 1</Tab>
         <Tab>wrong component 2</Tab>
       </Tabs>
@@ -141,6 +141,85 @@ describe('Tabs should not render other children then tablist or tabpanels', () =
     const tabs = queries.getByTestId('tabs');
 
     expect(tabs).toBeDefined();
-    expect(tabs.children).toBeEmptyDOMElement;
+    expect(tabs).toBeEmptyDOMElement;
+  });
+});
+
+describe('Tabs should comply with accessibility rules', () => {
+  it('traverse through tabs with keyboard', async () => {
+    const { tabs } = createTabs();
+
+    const tablist = tabs.firstChild as HTMLDivElement;
+    const tabpanels = tabs.lastChild as HTMLDivElement;
+
+    let tab1 = tablist.firstChild as HTMLButtonElement;
+    let tab3 = tablist.lastChild as HTMLButtonElement;
+
+    let tabpanel1 = tabpanels.firstChild as HTMLDivElement;
+    let tabpanel3 = tabpanels.lastChild as HTMLDivElement;
+
+    tab1.focus();
+
+    tab1 = tablist.firstChild as HTMLButtonElement;
+    tab3 = tablist.lastChild as HTMLButtonElement;
+    tabpanel1 = tabpanels.firstChild as HTMLDivElement;
+    tabpanel3 = tabpanels.lastChild as HTMLDivElement;
+
+    expect(tab1).toHaveFocus();
+    expect(tab1).toHaveClass('selected');
+    expect(tab1).not.toHaveClass('focussed');
+    expect(tab3).not.toHaveClass('selected');
+    expect(tab3).not.toHaveClass('focussed');
+    expect(tabpanel1).toHaveClass('selected');
+    expect(tabpanel3).not.toHaveClass('selected');
+
+    userEvent.keyboard('{ArrowLeft}');
+    userEvent.keyboard('{Space}'); // does not seem to do anything
+    userEvent.keyboard('{Enter}');
+
+    tab1 = tablist.firstChild as HTMLButtonElement;
+    tab3 = tablist.lastChild as HTMLButtonElement;
+    tabpanel1 = tabpanels.firstChild as HTMLDivElement;
+    tabpanel3 = tabpanels.lastChild as HTMLDivElement;
+
+    expect(tab3).toHaveFocus();
+    expect(tab3).toHaveClass('selected');
+    expect(tab1).not.toHaveClass('selected');
+    expect(tab1).not.toHaveClass('focussed');
+    expect(tabpanel3).toHaveClass('selected');
+    expect(tabpanel1).not.toHaveClass('selected');
+
+    userEvent.keyboard('{ArrowRight}');
+    userEvent.keyboard('{Enter}');
+
+    tab1 = tablist.firstChild as HTMLButtonElement;
+    tab3 = tablist.lastChild as HTMLButtonElement;
+    tabpanel1 = tabpanels.firstChild as HTMLDivElement;
+    tabpanel3 = tabpanels.lastChild as HTMLDivElement;
+
+    expect(tab1).toHaveFocus();
+    expect(tab1).toHaveClass('selected');
+    expect(tab3).not.toHaveClass('selected');
+    expect(tab3).not.toHaveClass('focussed');
+    expect(tabpanel1).toHaveClass('selected');
+    expect(tabpanel3).not.toHaveClass('selected');
+
+    userEvent.keyboard('{End}');
+
+    tab1 = tablist.firstChild as HTMLButtonElement;
+    tab3 = tablist.lastChild as HTMLButtonElement;
+
+    expect(tab3).toHaveFocus();
+    expect(tab3).toHaveClass('focussed');
+    expect(tab1).not.toHaveClass('focussed');
+
+    userEvent.keyboard('{Home}');
+
+    tab1 = tablist.firstChild as HTMLButtonElement;
+    tab3 = tablist.lastChild as HTMLButtonElement;
+
+    expect(tab1).toHaveFocus();
+    expect(tab1).toHaveClass('selected');
+    expect(tab3).not.toHaveClass('focussed');
   });
 });
