fd8a220a1aa9ff1e2424f261fde9db744cae15c8
Dirk Stals <dirkstals@gmail.com>
2022-05-23 11:11:07 +0200

    UCL-27 Use right imports

--- src/Tabs/Tab.test.tsx
+++ src/Tabs/Tab.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TabButton as Tab, Props } from './TabButton';
+import { Tab, Props } from './Tab';
 import { render } from '@testing-library/react';
 
 const defaultParams: Props = {
