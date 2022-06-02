f5e58156abaac5d4778dc432611c23b01c5de6a2
Kuba Słowikowski <slowikowski.kuba@gmail.com>
2022-06-02 12:14:42 +0200

    UCL-142 remove homepage url from logo link

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,7 +3,5 @@ import brandImage from './onewelcome.svg'
 
 export default create({
     brandTitle: "Onewelcome's storybook",
-    brandUrl: 'https://onewelcome.com',
     brandImage,
-    brandTarget: '_blank',
 })
