2137875880c12c21b025fceb0a28e2e7220eab77
Kuba Słowikowski <slowikowski.kuba@gmail.com>
2022-06-02 11:38:50 +0200

    UCL-142 change storybook logo to onewelcome's

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import {create} from "@storybook/theming"
+import brandImage from './onewelcome.svg'
+
+export default create({
+    brandTitle: "Onewelcome's storybook",
+    brandUrl: 'https://onewelcome.com',
+    brandImage,
+    brandTarget: '_blank',
+})
