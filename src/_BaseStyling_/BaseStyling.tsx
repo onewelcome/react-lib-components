63a3cc4faa98b4f459eb95e7d212c0b9a70001c1
Daniel <daanf@live.nl>
2022-09-30 10:43:52 +0200

    UCL-176 light grey border variable

--- src/_BaseStyling_/BaseStyling.tsx
+++ src/_BaseStyling_/BaseStyling.tsx
@@ -13,7 +13,6 @@ interface CSSProperties {
   buttonFillTextColor?: string;
   buttonFillBackgroundColor?: string;
   buttonOutlineHoverTextColor?: string;
-  inputBorderColor?: string;
   inputBorderRadius?: string;
   inputBorderWidth?: string;
   inputBorderWidthFocus?: string;
@@ -41,6 +40,7 @@ interface CSSProperties {
   error?: string;
   disabled?: string;
   greyedOut?: string;
+  lightGreyBorder?: string;
   warning?: string;
   fontFamily?: string;
   fontFamilyCode?: string;
@@ -72,7 +72,6 @@ export const BaseStyling = ({ children, properties = {} }: Props) => {
     buttonFillTextColor: "#fff",
     buttonFillBackgroundColor: "#fff",
     buttonOutlineHoverTextColor: "#fff",
-    inputBorderColor: "#e9e9eb",
     inputBorderRadius: "8px",
     inputBorderWidth: "1px",
     inputBorderWidthFocus: "2px",
@@ -100,6 +99,7 @@ export const BaseStyling = ({ children, properties = {} }: Props) => {
     error: "#d9291c",
     disabled: "#e9e9eb",
     greyedOut: "#6f6f76",
+    lightGreyBorder: "#e9e9eb",
     warning: "#ff6105",
     fontFamily: "'Red Hat Display', sans-serif",
     fontFamilyCode: "'Red Hat Mono', sans-serif",
