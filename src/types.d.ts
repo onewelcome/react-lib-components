602af76cb00d2c608e8e859eafff1bc557c912e3
Daniel <daanf@live.nl>
2022-10-04 08:23:33 +0200

    UCL-192 started with scss autocompletion

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-declare module "*.scss" {
-  const content: { [className: string]: string };
-  export default content;
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
 }
