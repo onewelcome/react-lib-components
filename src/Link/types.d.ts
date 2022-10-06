9eb581c2e2eb8305bc77708f20f429ae1925f1a0
Daniel <daanf@live.nl>
2022-10-06 11:42:08 +0200

    UCL-195 updated link component prop, it gave an error

--- src/Link/types.d.ts
+++
@@ -1,9 +0,0 @@
-import * as H from "history";
-
-export interface LinkProps<S = H.LocationState>
-  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  component?: React.ComponentType<unknown> | undefined;
-  to: H.LocationDescriptor<S> | ((location: H.Location<S>) => H.LocationDescriptor<S>);
-  replace?: boolean | undefined;
-  innerRef?: React.Ref<HTMLAnchorElement> | undefined;
-}
