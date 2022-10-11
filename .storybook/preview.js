4b072ff04be89fffa28f9ee9f22c586dfcdf2794
pawel-napieracz <pawel.napieracz@onegini.com>
2022-10-11 06:46:23 +0200

    UCL-112 Add docs view as defaul

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,17 @@
-import { BaseStyling } from '../src/_BaseStyling_/BaseStyling';
-import './base.scss';
+import { BaseStyling } from "../src/_BaseStyling_/BaseStyling";
+import "./base.scss";
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
+  viewMode: "docs",
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
-  actions: { argTypesRegex: '^on.*' },
+  actions: { argTypesRegex: "^on.*" }
 };
 
 export const decorators = [
-  (Story) => (
+  Story => (
     <BaseStyling>
       <Story />
     </BaseStyling>
-  ),
+  )
 ];
