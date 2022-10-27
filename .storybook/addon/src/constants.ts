6daef672cf604e4c0f313d93e1a7b2a439d6e543
Daniel <daanf@live.nl>
2022-10-27 10:21:32 +0200

    project setup

---
+++ .storybook/addon/src/constants.ts
@@ -0,0 +1,9 @@
+export const ADDON_ID = "storybook/basestyling";
+export const PANEL_ID = `${ADDON_ID}/basestyling`;
+export const PARAM_KEY = `BaseStyling`;
+
+export const EVENTS = {
+  RESULT: `${ADDON_ID}/result`,
+  REQUEST: `${ADDON_ID}/request`,
+  CLEAR: `${ADDON_ID}/clear`
+};
