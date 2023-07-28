f213ea9806af4e84789a248151eb138824833bf8
Daniel <daanf@live.nl>
2023-07-28 16:01:14 +0200

    Revert "UCL-352 updated toggle component to align with Quantum"
    This reverts commit 14fad9a966a3c12ddcdff78c2350af60c66a682f.

--- src/components/Form/Toggle/Toggle.test.tsx
+++ src/components/Form/Toggle/Toggle.test.tsx
@@ -32,16 +32,10 @@ const createToggle = (params?: (defaultParams: Props) => Props) => {
   }
   const queries = render(<Toggle {...parameters} data-testid="toggle"></Toggle>);
   const toggle = queries.getByTestId("toggle");
-  const toggleWrapper = toggle.closest(".toggle-wrapper");
-
-  if (!toggleWrapper) {
-    throw new Error("toggleWrapper is null");
-  }
 
   return {
     ...queries,
-    toggle,
-    toggleWrapper
+    toggle
   };
 };
 
@@ -101,100 +95,5 @@ describe("helperProps should be properly propagated down", () => {
     const helperTextAnchor = getByTestId("helpertextanchor");
 
     expect(helperTextAnchor).toBeTruthy();
-  });
-});
-
-describe("All the props", () => {
-  it("should have toggle-wrapper--left class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      labelPosition: "left"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--left");
-  });
-
-  it("should have toggle-wrapper--right class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      labelPosition: "right"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--right");
-  });
-
-  it("should have toggle-wrapper--hide-label class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      hideLabel: true
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--hide-label");
-  });
-
-  it("should have toggle-wrapper--nowrap class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      labelOverflow: "nowrap"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--nowrap");
-  });
-
-  it("should have toggle-wrapper--wrap class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      labelOverflow: "wrap"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--wrap");
-  });
-
-  it("should have toggle-wrapper--emphasis class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      version: "emphasis"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--emphasis");
-  });
-
-  it("should have toggle-wrapper--neutral class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      version: "neutral"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--neutral");
-  });
-
-  it("should have toggle-wrapper--packed class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      spacing: "packed"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--packed");
-  });
-
-  it("should have toggle-wrapper--between class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({
-      ...defaultParams,
-      spacing: "between"
-    }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--between");
-  });
-
-  it("should have toggle-wrapper--disabled class", () => {
-    const { toggleWrapper } = createToggle(defaultParams => ({ ...defaultParams, disabled: true }));
-
-    expect(toggleWrapper).toHaveClass("toggle-wrapper--disabled");
-  });
-
-  it("should have a label", () => {
-    const { getByText } = createToggle();
-
-    expect(getByText("Label")).toBeTruthy();
   });
 });
--- stories/Form/Elements/Toggle.stories.tsx
+++ stories/Form/Elements/Toggle.stories.tsx
@@ -63,11 +63,7 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = args => (
-  <div style={{ width: "200px" }}>
-    <ToggleComponent {...args} />
-  </div>
-);
+const Template: Story<Props> = args => <ToggleComponent {...args} />;
 
 export const Toggle = Template.bind({});
 
@@ -75,43 +71,4 @@ Toggle.args = {
   name: "Example toggle",
   label: "Toggle label",
   helperProps: { children: <a href="https://www.google.com">Test</a> }
-};
-
-export const ToggleLabelLeft = Template.bind({});
-
-ToggleLabelLeft.args = {
-  ...Toggle.args,
-  labelPosition: "left"
-};
-
-export const ToggleLabelHidden = Template.bind({});
-ToggleLabelHidden.args = {
-  ...Toggle.args,
-  hideLabel: true
-};
-
-export const ToggleLabelOverflow = Template.bind({});
-ToggleLabelOverflow.args = {
-  ...Toggle.args,
-  label: "This is a very long label that will overflow the toggle.",
-  labelOverflow: "wrap"
-};
-
-export const ToggleVersionNeutral = Template.bind({});
-ToggleVersionNeutral.args = {
-  ...Toggle.args,
-  version: "neutral",
-  checked: true
-};
-
-export const ToggleSpacingBetween = Template.bind({});
-ToggleSpacingBetween.args = {
-  ...Toggle.args,
-  spacing: "between"
-};
-
-export const ToggleDisabled = Template.bind({});
-ToggleDisabled.args = {
-  ...Toggle.args,
-  disabled: true
 };
