252f3587ceaa6447af426a73d0922657646a2b0b
michal.gorski <michal.gorski@onewelcome.com>
2023-12-29 08:28:39 +0100

    UCL-413 Added eslint-plugin-export-scope plugin to have control over exported code scopes

--- stories/types.d.ts
+++ stories/types.d.ts
@@ -23,3 +23,8 @@ declare module "*.module.scss" {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module "*.mdx" {
+  let MDXComponent: (props) => JSX.Element;
+  export default MDXComponent;
+}
