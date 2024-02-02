c1da1892ab2a3412e2c555fb6f4b249ad43a998b
pawel-napieracz <pawel.napieracz@onegini.com>
2024-02-02 14:01:55 +0100

    UCL-53 Fix interactions in MultiSelect story

--- stories/Form/Elements/MultiSelect.stories.tsx
+++ stories/Form/Elements/MultiSelect.stories.tsx
@@ -21,6 +21,7 @@ import MultiSelectDocumentation from "./MultiSelect.mdx";
 import { MultiOption } from "../../../src/components/Form/Select/MultiSelect/MultiOption";
 import { MultiSelectProps } from "../../../src/components/Form/Select/Select.interfaces";
 import { conditionalPlay } from "../../../.storybook/conditionalPlay";
+import { expect } from "@storybook/jest";
 import { userEvent, waitFor, within } from "@storybook/testing-library";
 
 const generateOptions = count => {
